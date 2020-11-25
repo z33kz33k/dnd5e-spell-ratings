2433cdda2a678e7d5e1b03ae614f2a3bdd4fb1b3
z33k <zeek@int.pl>
2020-11-26 00:49:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# dnd5e-spell-ratings
+DnD 5e spell ratings based on community guides' grades
---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+
+    DnD 5e spell rating
+
+    @author: z33k
+
+"""
+
+
+def run():
+    ...
+
+
+if __name__ == '__main__':
+    run()
