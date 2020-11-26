4abaa0f00a9dd710411a51345e7d837fdd3de937
z33k <zeek@int.pl>
2020-11-26 01:25:24 +0100

    Start work on parsing 5e.tool JSON data for spell names

---
+++ dnd5e_sr/__init__.py
@@ -0,0 +1,16 @@
+"""
+
+    dnd5e_sr
+    ~~~~~~~~~~~~~~~~~~~
+
+    DnD 5e spell ratings based on community guides' grades.
+
+    @author: z33k
+
+"""
+
+__author__ = "z33k"
+__contact__ = "zeek@int.pl"
+__version__ = "0.1.1.dev0"
+__description = f"{__name__} is a script that groks DnD 5e spell ratings from available " \
+                f"community guides"
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 """
 
-    DnD 5e spell rating
+    DnD 5e spell ratings
 
     @author: z33k
 
 """
 
+from dnd5e_sr.parse_data import parse
+
 
 def run():
-    ...
+    parse()
 
 
 if __name__ == '__main__':
